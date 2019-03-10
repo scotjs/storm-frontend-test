9e02ffd44bea99a765db60e57baa2862c8a56e18
ScotJS <scotnjs@gmail.com>
2019-03-10 13:59:52 +0000

    task 7 - mark task as done and persist

--- src/js/components/listItem.js
+++ src/js/components/listItem.js
@@ -6,7 +6,7 @@ const ListItem =
 Vue.component('listitem', {
     name: 'ListItem',
     props: ['task', 'tasks'],
-    template: '<li><input type="checkbox" v-model="isDone" v-on:click="markAsDone" v-bind:value="isDone" aria-label="done"/>{{task.title}} - {{isDone}}<a class="deleteLink" v-on:click="deleteTask">x</a></li>',
+    template: '<li><input type="checkbox" v-model="isDone" v-on:click="markAsDone" v-bind:value="isDone" aria-label="done"/>{{task.title}}<a class="deleteLink" v-on:click="deleteTask">x</a></li>',
     data: function(){
         return {
             isDone: this.task.isDone == true,
@@ -15,7 +15,17 @@ Vue.component('listitem', {
     },
     methods: {
         markAsDone: function(event){
-            console.log('patch');
+            loadProgressBar();
+            axios
+                .patch(`http://localhost:4000/api/task/${this.task.id}`, 
+                    {isDone: this.isDone == 0 ? "1" : "0"}
+                )
+                .then((response) => {   
+                    // console.log(response);
+                })
+                .catch(function(e){
+                    console.log(e);
+                });
         },
         deleteTask: function(event){
             if (event) {
